2797d361e226032faf8363dab08faed0ba3cb27e
Ludw1gd <144349814+Ludw1gd@users.noreply.github.com>
2024-03-30 22:42:25 +0700

    Update minute_log.sh

--- Soal_4/minute_log.sh
+++ Soal_4/minute_log.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
+#* * * * * /bin/bash /home/ludwigd/SisOP/Nomor4/minute_log.sh
+
+#0 * * * * /bin/bash /home/ludwigd/SisOP/Nomor4/aggregate_minutes_to_hourly_log.sh
+
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/home/ludwigd/SisOP/Nomor4
+
 timestamp=$(date +"%Y%m%d%H%M%S")
 
-mem_total=$(/usr/bin/free -m | /usr/bin/awk '/^Mem:/{print $2}')
-mem_used=$(/usr/bin/free -m | /usr/bin/awk '/^Mem:/{print $3}')
-mem_free=$(/usr/bin/free -m | /usr/bin/awk '/^Mem:/{print $4}')
-mem_shared=$(/usr/bin/free -m | /usr/bin/awk '/^Mem:/{print $5}')
-mem_buff=$(/usr/bin/free -m | /usr/bin/awk '/^Mem:/{print $6}')
-mem_available=$(/usr/bin/free -m | /usr/bin/awk '/^Mem:/{print $7}')
+mem_total=$(free -m | awk '/^Mem:/{print $2}')
+mem_used=$(free -m | awk '/^Mem:/{print $3}')
+mem_free=$(free -m | awk '/^Mem:/{print $4}')
+mem_shared=$(free -m | awk '/^Mem:/{print $5}')
+mem_buff=$(free -m | awk '/^Mem:/{print $6}')
+mem_available=$(free -m | awk '/^Mem:/{print $7}')
 
-swap_total=$(/usr/bin/free -m | /usr/bin/awk '/^Swap:/{print $2}')
-swap_used=$(/usr/bin/free -m | /usr/bin/awk '/^Swap:/{print $3}')
-swap_free=$(/usr/bin/free -m | /usr/bin/awk '/^Swap:/{print $4}')
+swap_total=$(free -m | awk '/^Swap:/{print $2}')
+swap_used=$(free -m | awk '/^Swap:/{print $3}')
+swap_free=$(free -m | awk '/^Swap:/{print $4}')
 
 path_user="/home/ludwigd/"
-path_size=$(/usr/bin/du -sh $path_user | /usr/bin/awk '{print $1}')
-echo "$mem_total,$mem_used,$mem_free,$mem_shared,$mem_buff,$mem_available,$swap_total,$swap_used,$swap_free,$path_user,$path_size" >> /home/ludwigd/SisOP/Nomor4/metrics_$timestamp.log
+path_size=$(du -sh $path_user | awk '{print $1}')
 
-chmod 600 "/home/ludwigd/SisOP/Nomor4/metrics_$timestamp.log"
+echo "$mem_total,$mem_used,$mem_free,$mem_shared,$mem_buff,$mem_available,$swap_total,$swap_used,$swap_free,$path_user,$path_size" >> /home/ludwigd/SisOP/Nomor4/log/metrics_$timestamp.log
+
+chmod 600 "/home/ludwigd/SisOP/Nomor4/log/metrics_$timestamp.log"
